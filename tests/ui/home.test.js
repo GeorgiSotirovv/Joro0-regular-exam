08bbf31e0a602d52a446d15c5a73001ea163cf3e
GeorgiSotirovv <tirko123456@abv.bg>
2024-04-13 15:52:59 +0300

    Fixed UI tests

--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -1,9 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
-test('Check home page', async ({ page }) => {
-    await page.goto('http://localhost:8080/');
-    const heading = await page.$('form');
-    const text = await heading.textContent();
-    expect(text).toContain('Welcome to my cooking class');  
+test('Check header', async ({ page }) => {
+    await page.goto('http://localhost:8080'); 
+    const homeLink = await page.$('a[href="/"]');
+    const text = await homeLink.textContent();
+    expect(text).toBe('Home');
   });
   
